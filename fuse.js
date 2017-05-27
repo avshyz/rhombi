8a6d31b74bdce63e9db5ea25c301ea9134a0fab0
Avishay Zarad <avishay@singular.net>
2017-05-27 17:27:07 +0300

    initial, boilerplate stuffies

---
+++ fuse.js
@@ -0,0 +1,22 @@
+const { FuseBox, SVGPlugin, CSSPlugin, BabelPlugin } = require("fuse-box");
+
+// FuseBox 2.0 is coming soon!
+// http://fuse-box.org:3333/
+
+let fuse = new FuseBox({
+    homeDir: "src/",
+    sourcemaps: true,
+    output: "build/$name.js",
+    plugins: [
+        SVGPlugin(),
+        CSSPlugin(),
+        BabelPlugin()
+    ]
+});
+
+fuse.dev();
+fuse.bundle("app")
+    .watch().hmr()
+    .instructions("> index.jsx");
+
+fuse.run();
